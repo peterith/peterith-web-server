d3179dfdec5729c365dc460604714ad94d669661
Peerapong Rithisith <p.rithisith@hotmail.com>
2019-10-24 00:51:39 +0100

    user can deactivate account

--- src/utils/authentications.js
+++ src/utils/authentications.js
@@ -3,19 +3,23 @@ import jwt from 'jsonwebtoken';
 
 export const authenticateUser = (User, username, password) => {
   return new Promise(async (resolve, reject) => {
-    const user = await User.findOne({
-      username
-    });
+    try {
+      const user = await User.findOne({
+        username
+      });
 
-    if (!user) {
-      reject('Username is not registered');
+      if (!user) {
+        reject('Username is not registered');
+      }
+
+      if (await bcrypt.compare(password, user.password)) {
+        resolve();
+      }
+
+      reject('Incorrect password');
+    } catch (error) {
+      reject(error);
     }
-
-    if (await bcrypt.compare(password, user.password)) {
-      resolve();
-    }
-
-    reject('Incorrect password');
   });
 };
 
